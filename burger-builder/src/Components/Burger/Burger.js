ebc6483c2b6a89f79da1fe8389a38d0bfbf10758
ayush.sharma <ayush.sharma@hashedin.com>
2020-03-21 17:52:38 +0530

    added user friendly messages

--- burger-builder/src/Components/Burger/Burger.js
+++ burger-builder/src/Components/Burger/Burger.js
@@ -3,17 +3,22 @@ import React from 'react'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 const burger = (props) => {
-    const transformedIngredient = Object.keys(props.ingredients).map(
+    let transformedIngredient = Object.keys(props.ingredients).map(
        igkey =>{
          return [...Array(props.ingredients[igkey])].map((_, i)=>{
              return <BurgerIngredient key={igkey + i} type = {igkey}/>
-         }) 
-       } 
-    )
+         })
+       } ).reduce((arr,cur)=>{
+        return arr.concat(cur)
+     },[])
+
+       if(transformedIngredient.length === 0){
+        transformedIngredient = <div>Please add Ingreadients</div>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            {console.log(transformedIngredient)}
             {transformedIngredient}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
